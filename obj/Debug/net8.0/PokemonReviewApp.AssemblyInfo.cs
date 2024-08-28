c1c04047e7101783a1666a2cf1e9537764541005
Torchie1455 <Torchie1455@gmail.com>
2024-08-28 17:13:25 -0500

    updated build

--- obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88699b314d6980ae1161ef79c0dc946356d87f8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88b868a883b47d352ac73084d946dfcf348c2b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
